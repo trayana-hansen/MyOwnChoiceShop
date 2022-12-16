ed51fe6b6eca6040e613d7e1fb4bba1247ce2053
Trayana Hansen <92487819+trayana-hansen@users.noreply.github.com>
2022-12-16 12:11:14 +0100

    login and register server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
+import { fileURLToPath} from 'url'
 
 //Import routers
 import InitRouter from './Router/init.router.js'
@@ -11,8 +13,13 @@ import ReviewRouter from './Router/review.router.js'
 import ProductRouter from './Router/product.router.js'
 import { router as AuthRouter } from './Router/authenticate.router.js'
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
-app.use(express.urlencoded({ extended: true}))
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(__dirname + '/views'));
 
 dotenv.config();
 
@@ -21,12 +28,27 @@ const port = process.env.PORT || 4500
 //const api_key = process.env.APIKEY;
 
 
+app.set('view-engine', 'ejs')
 
 app.get('/', (req, res) => {
-	res.send('Hi')
+	res.render('index.ejs')
 })
 
-//Use routers, it cannot be put before it is called
+app.get('/login', (req, res) => {
+	res.render('login.ejs')
+})
+app.post('/login', (req, res) => {
+
+})
+
+app.get('/register', (req, res) => {
+	res.render('register.ejs')
+})
+app.post('/register', (req, res) => {
+
+})
+
+//Use routers, it cannot be used before it is called
 app.use(InitRouter)
 app.use(GenderRouter)
 app.use(TypeRouter)
@@ -40,3 +62,5 @@ app.use(AuthRouter)
 app.listen(4500, () => {
 	console.log(`The server is running on localhost:${port}`);
 });
+
+
